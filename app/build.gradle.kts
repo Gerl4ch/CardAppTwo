3bf104a8a5f9353aefef8816c83f2bb6f1f281e5
Gerl4ch <leonardohg2005@gmail.com>
2024-06-13 22:14:51 -0300

    Card Apps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
